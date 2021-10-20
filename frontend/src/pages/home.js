4298953d74360f56379f59d1e79a00944c8dc482
senseisub <seunsuberu85@gmail.com>
2021-10-20 14:48:05 -0500

    Added Search Bar component. Improved styling elements

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import "../styles/home.css"
 import Button from "../components/genericButton";
+import SearchBar from "../components/searchBar";
 
 export default function Home(props){
     return (
@@ -9,7 +10,10 @@ export default function Home(props){
                 <img className={"mainImage"} src={"https://s.abcnews.com/assets/dtci/elections/images/fantasymap-intro.png"}/>
                 <h1 className={"main-description"}>Learn about political races and interact with maps to see how things can play out!</h1>
                 <div className={"divForButton"}>
-                    <Button mainText={"Maps"} baseColor={"black"} textColor={"white"}/>
+                    <div>
+                        <SearchBar placeHolder={"Search for Locations"} baseColor={"white"} textColor={"black"} dropShadow={true}/>
+                    </div>
+                    <Button mainText={"All Maps"} baseColor={"black"} textColor={"white"} dropShadow={true}/>
                 </div>
             </main>
         </>
