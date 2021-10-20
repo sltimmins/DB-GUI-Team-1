4541ec382133ceb6a513326cd52185944638d5a2
Sam Timmins <stimmins@smu.edu>
2021-10-20 17:49:27 -0500

    Login and registration page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import './App.css';
 import axios from 'axios';
 import {
@@ -7,99 +7,57 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Users from "./Users.js"
 import Home from "./pages/home";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import {MainBackgroundColor, MainTitle} from "./constants/constants";
+import { AppContext, useProvideAppContext, setupLogin } from "./AppContext.js";
 
 // React functional component
-function App () {
-  // state for storage of the information on the webpage of forms and list, uses hooks
-  const [number, setNumber] = useState("")
-  const [values, setValues] = useState([])
+export function App () {
+  // Global context
+  let context = useProvideAppContext();
 
-  // ENTER YOUR EC2 PUBLIC IP/URL HERE
-  const ec2_url = ''
-  // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
-  const ec2 = false;
-  // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
-  const url = ec2 ? ec2_url : 'localhost'
-
-  // handle input field state change
-  const handleChange = (e) => {
-    setNumber(e.target.value);
-  }
-
-  const fetchBase = () => {
-    axios.get(`http://${url}:8000/`).then((res)=>{
-      alert(res.data);
-    })
-  }
-
-  // fetches vals of db via GET request
-  const fetchVals = () => {
-    axios.get(`http://${url}:8000/values`).then(
-      res => {
-        const values = res.data.data;
-        console.log(values);
-        setValues(values)
-    }).catch(err => {
-      console.log(err)
-    });
-  }
-
-  // handle input form submission to backend via POST request
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let prod = number * number;
-    axios.post(`http://${url}:8000/multplynumber`, {product: prod}).then(res => {
-      console.log(res);
-      fetchVals();
-    }).catch(err => {
-      console.log(err)
-    });
-    setNumber("");
-  }
-
-  // handle intialization and setup of database table, can reinitialize to wipe db
-  const reset = () => {
-    axios.post(`http://${url}:8000/reset`).then(res => {
-      console.log(res);
-      fetchVals();
-    }).catch(err => {
-      console.log(err)
-    });
+  // handle signout
+  const signout = () => {
+    localStorage.setItem('jwt', "")
   }
 
   // tell app to fetch values from db on first load (if initialized)
   useEffect(() => {
-    fetchVals();
+    setupLogin(context);
   }, [])
 
   return (
-    <>
-      <Router>
-      <div>
-        <Header baseColor={MainBackgroundColor}
-            routes={
-              [
-                {name: "Home", href: '/', active: true},
-                {name: "Maps", href: '/', active: false},
-                {name: "About", href: '/', active: false}
-              ]
-            } 
-            mainTitle={MainTitle} mainImage = {{src: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png", width: "70px", height: '50px'}}/>
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-        <Footer mainTitle={MainTitle}/>
+    <AppContext.Provider value={context}>
+      <div class="container">
+        <Router>
+          <div>
+            <Header baseColor={MainBackgroundColor}
+                routes={
+                  [
+                    {name: "Home", href: '/', active: true},
+                    {name: "Maps", href: '/', active: false},
+                    {name: "About", href: '/', active: false}
+                  ]
+                } 
+                mainTitle={MainTitle} mainImage = {{src: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png", width: "70px", height: '50px'}}/>
+            {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/login">
+                <Users />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+            <Footer mainTitle={MainTitle}/>
+          </div>
+        </Router>
       </div>
-    </Router>
-    </>
+    </AppContext.Provider>
   );
 }
 
---
+++ frontend/src/AppContext.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { axiosJWTHeader } from './utils/index.js'
+import axios from "axios";
+
+export const AppContext = React.createContext("app");
+
+export function useProvideAppContext() {
+    // ENTER YOUR EC2 PUBLIC IP/URL HERE
+    const ec2_url = "";
+    // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
+    const ec2 = false;
+    // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
+    const baseURL = ec2 ? ec2_url : "http://localhost:8000";
+
+    const [JWT, setJWT] = useState(null);
+    const [user, setUser] = useState(null);
+
+    const [setup, setSetup] = useState(false);
+
+    const signout = () => {
+        localStorage.setItem('jwt', "")
+    }
+
+
+    return {
+        user,
+        setUser,
+
+        JWT,
+        setJWT,
+
+        baseURL,
+
+        setup,
+        setSetup,
+
+        signout
+    }
+}
+
+export function setupLogin(context) {
+    // let stored = localStorage.getItem('jwt')
+    // if (stored) {
+    //     context.setJWT(stored)
+    //     axios
+    //         .get(context.baseURL + "/users/check", { headers: axiosJWTHeader(stored) })
+    //         .then((res) => {
+    //             //Set the user to the object we just got
+    //             context.setUser(res.data)
+    //             context.setSetup(true);
+    //         }).catch(() => {
+    //             context.setSetup(true);
+    //         })
+    // } else {
+    //     context.setSetup(true);
+    // }
+}
