3490e8a9369512d228b71778913d8a71a49d3c67
Sam Timmins <stimmins@smu.edu>
2021-11-01 22:17:46 -0500

    Working on responsive styles

--- frontend/src/pages/candidateCard.js
+++ frontend/src/pages/candidateCard.js
@@ -5,10 +5,10 @@ export default function CandidateCard ({ candidate }) {
     let name = candidate.firstName + " " + candidate.lastName;
     return (
         <div class="candidateCard">
-            <img class="candidateImage" alt={name} src={"http://placehold.it/250x250"} width={250} height={250} />
+            <img class="candidateImage" alt={name} src={"http://placehold.it/250x250"}/>
             <div class="info">
                 <h2>{name}</h2>
-                <p class="party" val={candidate.party}>{candidate.party}</p>
+                <p class="party">{candidate.party}</p>
             </div>
             <div class="clear"></div>
         </div>
