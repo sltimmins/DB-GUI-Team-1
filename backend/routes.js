3c868c2d9bd1f013606add679e443787ea1f7370
Alexander Shockley <ashockley@smu.edu>
2021-11-18 09:37:11 -0600

    Added functionality for saving CSV, getting custom election years, and getting valid election Years

--- backend/routes.js
+++ backend/routes.js
@@ -1,6 +1,10 @@
 const pool = require('./db');
 const jwt = require('jsonwebtoken');
 const bcrypt = require("bcryptjs");
+const fastcsv = require("fast-csv");
+const path = require("path");
+const fs = require("fs");
+const { waitForDebugger } = require('inspector');
 
 module.exports = function routes(app, logger) {
   // GET /
@@ -8,95 +12,8 @@ module.exports = function routes(app, logger) {
     res.status(200).send('Go to 0.0.0.0:3000.');
   });
 
-  // POST /reset
-  app.post('/reset', (req, res) => {
-    // obtain a connection from our pool of connections
-    pool.getConnection(function (err, connection){
-      if (err){
-        console.log(connection);
-        // if there is an issue obtaining a connection, release the connection instance and log the error
-        logger.error('Problem obtaining MySQL connection', err)
-        res.status(400).send('Problem obtaining MySQL connection'); 
-      } else {
-        // if there is no issue obtaining a connection, execute query
-        connection.query('drop table if exists test_table', function (err, rows, fields) {
-          if (err) { 
-            // if there is an error with the query, release the connection instance and log the error
-            connection.release()
-            logger.error("Problem dropping the table test_table: ", err); 
-            res.status(400).send('Problem dropping the table'); 
-          } else {
-            // if there is no error with the query, execute the next query and do not release the connection yet
-            connection.query('CREATE TABLE `db`.`test_table` (`id` INT NOT NULL AUTO_INCREMENT, `value` VARCHAR(45), PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);', function (err, rows, fields) {
-              if (err) { 
-                // if there is an error with the query, release the connection instance and log the error
-                connection.release()
-                logger.error("Problem creating the table test_table: ", err);
-                res.status(400).send('Problem creating the table'); 
-              } else { 
-                // if there is no error with the query, release the connection instance
-                connection.release()
-                res.status(200).send('created the table'); 
-              }
-            });
-          }
-        });
-      }
-    });
-  });
-
-  // POST /multplynumber
-  app.post('/multplynumber', (req, res) => {
-    console.log(req.body.product);
-    // obtain a connection from our pool of connections
-    pool.getConnection(function (err, connection){
-      if(err){
-        // if there is an issue obtaining a connection, release the connection instance and log the error
-        logger.error('Problem obtaining MySQL connection',err)
-        res.status(400).send('Problem obtaining MySQL connection'); 
-      } else {
-        // if there is no issue obtaining a connection, execute query and release connection
-        connection.query('INSERT INTO `db`.`test_table` (`value`) VALUES(\'' + req.body.product + '\')', function (err, rows, fields) {
-          connection.release();
-          if (err) {
-            // if there is an error with the query, log the error
-            logger.error("Problem inserting into test table: \n", err);
-            res.status(400).send('Problem inserting into table'); 
-          } else {
-            res.status(200).send(`added ${req.body.product} to the table!`);
-          }
-        });
-      }
-    });
-  });
-
-  // GET /checkdb
-  app.get('/values',  (req, res) => {
-    // obtain a connection from our pool of connections
-    pool.getConnection(function (err, connection){
-      if(err){
-        // if there is an issue obtaining a connection, release the connection instance and log the error
-        logger.error('Problem obtaining MySQL connection',err)
-        res.status(400).send('Problem obtaining MySQL connection'); 
-      } else {
-        // if there is no issue obtaining a connection, execute query and release connection
-        connection.query('SELECT value FROM `db`.`test_table`', function (err, rows, fields) {
-          connection.release();
-          if (err) {
-            logger.error("Error while fetching values: \n", err);
-            res.status(400).json({
-              "data": [],
-              "error": "Error obtaining values"
-            })
-          } else {
-            res.status(200).json({
-              "data": rows
-            });
-          }
-        });
-      }
-    });
-  });
+  
+ 
   /*
   route to create a new user account and add it to the database
   Uses bcrypt to encrypt the user's password
@@ -163,8 +80,76 @@ module.exports = function routes(app, logger) {
         connection.release();
     })
   });
+  
+  app.get('/customElections', authenticateToken, async (req,res) => {
+    pool.getConnection(function(err,connection) {
+      try {
+      connection.query("Select accountNumber FROM users WHERE username = ?", req.user.username, function(err,result,fields) {
+        accountNums = JSON.parse(JSON.stringify(result))
+          connection.query('select name,year from elections where createdBy = ?', accountNums[0]['accountNumber'], function(err,result,fields) {
+            res.send(JSON.parse(JSON.stringify(result)))
+          })
+        
+      })
+    } catch {
+      logger.error("Error getting custom election for username: " + req.user.username)
+    }
+      connection.release()
+    })
+  })
+  app.get('/validElectionYears', (req,res) => {
+    pool.getConnection(function(err,connection) {
+      connection.query("select accountNumber from users where username = ?", req.param('username'), function(err,result,fields) {
+        try {
+        connection.query("select year from elections where createdBy = ?",JSON.parse(JSON.stringify(result))[0]['accountNumber'],function(err,result,fields) {
+          try {
+          if(err) {
+            logger.error(err)
+            res.status(400).send('Oopsies')
+          } else {
+            res.send(JSON.parse(JSON.stringify(result)))
+          }
+        } catch {
+          logger.error('No years associated with that account')
+        }
+        })
+      } catch {
+        logger.error('No elections associated with that username')
+        res.send("No elections associated with the username: " + req.param('username'))
+      }
+      })
+      connection.release()
+    })
+  })
+  app.get('/saveCSV',(req,res) => {
+    const ws = fs.createWriteStream("Temp.csv");
+    pool.getConnection( function(err,connection) {
+      connection.query('select * from users', function(err,result) {
 
+        const jsonData = JSON.parse(JSON.stringify(result))
+        console.log(jsonData)
+        fastcsv
+        .write(jsonData, { headers: true })
+        .on("finish", function() {
+        console.log("Write to bezkoder_mysql_fastcsv.csv successfully!");
+        }).pipe(ws)
+        res.sendFile(path.join(__dirname, '/Temp.csv'))
+          // try {
+          //   fs.unlinkSync(path.join(__dirname,'/Temp.csv'))
+          //   console.log('file Deleted')
+          //   //file removed
+          // } catch(err) {
+          //   console.log('ERROR DELETING FILE')
+          //   console.log(err)
+          // }
+        
+        
+        
+      })
+      connection.release()
+    })
 
+  })
 
   /*
     Route that tests the JWT functionality, For future just use middleware authenticateToken
