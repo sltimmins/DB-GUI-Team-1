e7ea1c66e11235dd3de04e2f9bc841fe37da1129
EC2 Default User <ec2-user@ip-172-31-26-8.us-east-2.compute.internal>
2021-12-02 06:40:31 +0000

    Correct ec2 url

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,7 +1,7 @@
 export const MAIN_BACKGROUND_COLOR = "#f5f5f5";
 export const MAIN_TITLE = "Politimaps";
 // ENTER YOUR EC2 PUBLIC IP/URL HERE
-export const EC2_URL = "ec2-18-219-94-205.us-east-2.compute.amazonaws.com:8000";
+export const EC2_URL = "http://ec2-18-219-94-205.us-east-2.compute.amazonaws.com:8000";
 // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
 export const EC2 = true;
 // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
